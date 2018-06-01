6a3790bd60aa3e76fbc87ec864e77665ad76116d
Evan Latiolais <evanlatch99@gmail.com>
2018-06-01 15:54:50 -0500

    Changed and added to comments

--- SpreadSheet Parser/SpreadSheet_Parser.py
+++ SpreadSheet Parser/SpreadSheet_Parser.py
@@ -1,12 +1,14 @@
 import pip
 import setuptools
-import xlrd 
 import os
 import os.path 
 import datetime
 import csv
 import sys
 
+
+# AT A LATER TIME I WOULD LIKE TO IMPLEMENT THE GOOGLE SHEETS API
+# FOR NOW< I AM JUST WORKING OUT THE MATH OF GETTING TH ECORRECT SERIAL NUMBERS
 
 maxInt = sys.maxsize
 decrement = True
@@ -21,7 +23,7 @@ while decrement:
     except OverflowError:
         maxInt = int(maxInt/2)
         decrement = True
-
+        
 
 
 # Read the file
@@ -33,6 +35,8 @@ with open(r"C:\Users\ELATIOLAIS\Downloads\Testing reports.csv" , newline='') as 
            print (row)
        
       
-# need to have scrpt grab the correspondingserial number with the date
+# need to have script grab the corresponding serial number with the date
+# if(date == today's date), then isolate all rows containing today's date
+# after, grab all of the serial numbers that start with "AMC" and put them into an array
 # determine the serial number column by matching it with the (0,SN_COLUMN)
 # global variables: SN_COLUMN, CAM_SN
