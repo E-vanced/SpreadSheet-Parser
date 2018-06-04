ef14c21b7779be32c0d05fe073da6ce038a8f532
Evan Latiolais <evanlatch99@gmail.com>
2018-06-04 15:15:43 -0500

    Updated comments

--- SpreadSheet Parser/SpreadSheet_Parser.py
+++ SpreadSheet Parser/SpreadSheet_Parser.py
@@ -7,13 +7,12 @@ import sys
 
 from datetime import date
 
-# today.day, today.month, today.year
 today = date.today()
 print("Today's date is", today.strftime("%#m/%#d/%Y"))
 
 
 # AT A LATER TIME I WOULD LIKE TO IMPLEMENT THE GOOGLE SHEETS API
-# FOR NOW< I AM JUST WORKING OUT THE MATH OF GETTING TH ECORRECT SERIAL NUMBERS
+# FOR NOW I AM JUST WORKING OUT THE MATH OF GETTING THE CORRECT SERIAL NUMBERS
 
 # r"C:\Users\ELATIOLAIS\Downloads\cameras-report.csv"
 # r"C:\Users\ELATIOLAIS\Downloads\Testing reports.csv"
@@ -24,8 +23,8 @@ with open(r"C:\Users\ELATIOLAIS\Downloads\Testing reports.csv" , newline='') as 
             print(row['DATE'], row['SERIAL NUMBER'])
 
 
-# need to have script grab the corresponding serial number with the date
-# if(date == today's date), then isolate all rows containing today's date
+# DONE ------ need to have script grab the corresponding serial number with the date
+# DONE ------ if(date == today's date), then isolate all rows containing today's date
 # after, grab all of the serial numbers that start with "AMC" and put them into an array
-# determine the serial number column by matching it with the (0,SN_COLUMN)
-# global variables: SN_COLUMN, CAM_SN
+# have user input file path via drag and drop because end user accessiblity
+# have ability for to user to select month, day, and year previous to current date
