33fd18daace8d0f5de99b3c3b96981ad21f36c41
Evan Latiolais <evanlatch99@gmail.com>
2018-06-04 10:30:17 -0500

    separated desired tasks into functions, added date module

--- SpreadSheet Parser/SpreadSheet_Parser.py
+++ SpreadSheet Parser/SpreadSheet_Parser.py
@@ -2,39 +2,46 @@ import pip
 import setuptools
 import os
 import os.path 
-import datetime
 import csv
 import sys
 
+from datetime import date
 
+def main():
+    getDate()
+    getSN()
+
+
+def getDate():
+    # today.day, today.month, today.year
+    today = date.today()
+    print("Today's date is", today.strftime("%m/%d/%Y"))
+
+def getSN():
 # AT A LATER TIME I WOULD LIKE TO IMPLEMENT THE GOOGLE SHEETS API
 # FOR NOW< I AM JUST WORKING OUT THE MATH OF GETTING TH ECORRECT SERIAL NUMBERS
 
-maxInt = sys.maxsize
-decrement = True
+    maxInt = sys.maxsize
+    decrement = True
 
-while decrement:
-    # decrease the maxInt value by factor 10 
+    while decrement:
+    # decrease the maxInt value by factor 10
     # as long as the OverflowError occurs.
+        decrement = False
+        try:
+           csv.field_size_limit(maxInt)
+        except OverflowError:
+           maxInt = int(maxInt / 2)
+           decrement = True
+    # Read the file
+    # r"C:\Users\ELATIOLAIS\Downloads\cameras-report.csv"
+    # r"C:\Users\ELATIOLAIS\Downloads\Testing reports.csv"
+    with open(r"C:\Users\ELATIOLAIS\Downloads\Testing reports.csv" , newline='') as csvfile:
+        reportReader = csv.DictReader(csvfile,)
+        for row in reportReader:
+            print(row)
 
-    decrement = False
-    try:
-        csv.field_size_limit(maxInt)
-    except OverflowError:
-        maxInt = int(maxInt/2)
-        decrement = True
-        
 
-
-# Read the file
-# r"C:\Users\ELATIOLAIS\Downloads\cameras-report.csv"
-# r"C:\Users\ELATIOLAIS\Downloads\Testing reports.csv"
-with open(r"C:\Users\ELATIOLAIS\Downloads\Testing reports.csv" , newline='') as csvfile:
-       reportReader= csv.DictReader(csvfile,)
-       for row in reportReader:
-           print (row)
-       
-      
 # need to have script grab the corresponding serial number with the date
 # if(date == today's date), then isolate all rows containing today's date
 # after, grab all of the serial numbers that start with "AMC" and put them into an array
