5dd9c617146bef1b7d2ac867bb351514479343d0
Evan Latiolais <evanlatch99@gmail.com>
2018-06-04 11:31:58 -0500

    updated to print only date and Serial number, got rid of maxSize field adjustment

--- SpreadSheet Parser/SpreadSheet_Parser.py
+++ SpreadSheet Parser/SpreadSheet_Parser.py
@@ -15,25 +15,12 @@ print("Today's date is", today.strftime("%m/%d/%Y"))
 # AT A LATER TIME I WOULD LIKE TO IMPLEMENT THE GOOGLE SHEETS API
 # FOR NOW< I AM JUST WORKING OUT THE MATH OF GETTING TH ECORRECT SERIAL NUMBERS
 
-maxInt = sys.maxsize
-decrement = True
-
-while decrement:
-# decrease the maxInt value by factor 10
-# as long as the OverflowError occurs.
-    decrement = False
-    try:
-        csv.field_size_limit(maxInt)
-    except OverflowError:
-        maxInt = int(maxInt / 2)
-        decrement = True
-# Read the file
 # r"C:\Users\ELATIOLAIS\Downloads\cameras-report.csv"
 # r"C:\Users\ELATIOLAIS\Downloads\Testing reports.csv"
 with open(r"C:\Users\ELATIOLAIS\Downloads\Testing reports.csv" , newline='') as csvfile:
     reportReader = csv.DictReader(csvfile,)
     for row in reportReader:
-        print(row)
+        print(row['DATE'], row['SERIAL NUMBER'])
 
 
 # need to have script grab the corresponding serial number with the date
